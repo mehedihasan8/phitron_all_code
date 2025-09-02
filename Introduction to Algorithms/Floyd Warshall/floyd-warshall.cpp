c335d6fcdb1305d8053673bd7bd253dec077f528
mehedihasan8 <mehedihasanfoysal22@gmail.com>
2025-09-02 12:07:58 +0600

    added floyd warshall

---
+++ Introduction to Algorithms/Floyd Warshall/floyd-warshall.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, e;
+int adj_mat[1005][1005];
+
+int main()
+{
+    cin >> n >> e;
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j)
+                adj_mat[i][j] = 0;
+            else
+                adj_mat[i][j] = INT_MAX;
+        }
+
+    while (e--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        adj_mat[a][b] = c;
+    }
+
+    for (int k = 0; k < n; k++)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (adj_mat[i][k] != INT_MAX && adj_mat[k][j] != INT_MAX &&
+                    adj_mat[i][k] + adj_mat[k][j] < adj_mat[i][j])
+                {
+                    adj_mat[i][j] = adj_mat[i][k] + adj_mat[k][j];
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (adj_mat[i][j] == INT_MAX)
+                cout << "INT ";
+            else
+                cout << adj_mat[i][j] << " ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
